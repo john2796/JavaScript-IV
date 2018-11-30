7d47bbeaf6602ed0aaf04ebeb975eea41e357919
john2796 <benedictmiranda27@yahoo.com>
2018-11-29 16:48:52 -0800

    done

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -1,4 +1,3 @@
-
 class Person {
   constructor(props) {
     this.name = props.name;
@@ -57,8 +56,9 @@ class Student extends Person {
   }
 
   graduate(cb) {
-    if (cb() > 70) {
-      return `${this.name} your finall grade is ${this.grade}% you are ready to GRADUATE congratulations🎉`;
+    this.grade = cb()
+    if (this.grade > 70) {
+      return `${this.name} your final grade is ${this.grade}% you are ready to GRADUATE congratulations🎉`;
     }
     return `your grade is ${this.grade}% , you need more practice !!!😹`;
   }
@@ -110,16 +110,4 @@ john.listsSubjects(john);
 john.pRAssignment(john, 'REACT');
 john.sprintChallenge(john, 'OOP');
 jordan.studentGrade(john)
-john.graduate(jordan.studentGrade());
-
-
-
-
-// #### Stretch Problem
-
-// * Extend the functionality of the Student by adding a prop called grade and setting it equal to a number between 1-100.
-
-// * Now that our students have a grade build out a method on the Instructor (this will be used by _BOTH_ instructors and PM's) that will randomly add or subtract points to a student's grade. _Math.random_ will help.
-// * Add a graduate method to a student.
-//   * This method, when called, will check the grade of the student and see if they're ready to graduate from Lambda School
-//   * If the student's grade is above a 70% let them graduate! Otherswise go back to grading their assignments to increase their score.
+john.graduate(jordan.studentGrade)
